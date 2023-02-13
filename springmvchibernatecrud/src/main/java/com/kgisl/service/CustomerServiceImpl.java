033b4c68e479d7f795ea20f9661c0f7543e7d1bf
Babuprasath02 <81895758+Babuprasath02@users.noreply.github.com>
2023-02-13 16:38:58 +0530

    Add files via upload

---
+++ springmvchibernatecrud/src/main/java/com/kgisl/service/CustomerServiceImpl.java
@@ -0,0 +1,42 @@
+package com.kgisl.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kgisl.dao.CustomerDao;
+import com.kgisl.model.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+    @Autowired
+    private CustomerDao customerDAO;
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+        return customerDAO.getCustomers();
+    }
+
+    @Override
+    @Transactional
+    public void saveCustomer(Customer theCustomer) {
+        customerDAO.saveCustomer(theCustomer);
+    }
+
+    @Override
+    @Transactional
+    public Customer getCustomer(int theId) {
+        return customerDAO.getCustomer(theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteCustomer(int theId) {
+        customerDAO.deleteCustomer(theId);
+    }
+    
+}
